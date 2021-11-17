eee587f44f2e7f23ab8c57591a2f7300a12c0685
kinghyeongu <guarns1234@gmail.com>
2021-11-18 02:38:15 +0900

    상세사진 개수에 맞게 이미지 링크 출력

--- snap_musinsa/snap_musinsa.py
+++ snap_musinsa/snap_musinsa.py
@@ -16,17 +16,35 @@ while PageNum <= 1: # 페이지 넘버를 사용해 자동적으로 페이지가
     ImgNum = 0
     while ImgNum < 60:
         driver.find_elements_by_css_selector('.articleImg')[ImgNum].click() # 이미지 접속
-        time.sleep(2)
+        #time.sleep(2)
 
         link = driver.current_url
         res = requests.get(link, headers=headers)
         res.raise_for_status()
         soup = BeautifulSoup(res.text)
 
-        listItem = soup.select(".styleItem-box")
-        print(listItem)
-        #itemlink = listItem.select_one('a').get('href')
+        listItem = soup.select_one(".styleItem-box")
+        #print(listItem)
+        a = listItem.select('a')
+        itemname = listItem.select('p')
+        
+        '''
+        deitemimg1 = re.search('href="(.+?)"',str(a[0]))
+        if deitemimg1:
+            deitemimg1 = deitemimg1.group(1)
 
+        deitemimg2 = re.search('href="(.+?)"',str(a[5]))
+        if deitemimg2:
+            deitemimg2 = deitemimg2.group(1)
+
+        deitemimg3 = re.search('href="(.+?)"',str(a[10]))
+        if deitemimg3:
+            deitemimg3 = deitemimg3.group(1)
+
+        deitemimg4 = re.search('href="(.+?)"',str(a[10]))
+        if deitemimg4:
+            deitemimg4 = deitemimg4.group(1)
+        '''
         tbody = soup.select_one('tbody')
 
         span = tbody.select('span')
@@ -41,7 +59,28 @@ while PageNum <= 1: # 페이지 넘버를 사용해 자동적으로 페이지가
         url = driver.find_elements_by_css_selector('.lbox')[0].get_attribute('href') # 이미지 URL 따기
         print('이미지url : ', url)
 
-        #print('상세 제품 링크 :', 'https:',listItem)
+        #for문 사용시 json 변환 어려울 것 같음.
+        cnt = 0
+        for k in range(len(a)):
+            if(re.search('title(.+?)"', str(a[k]))):
+                cnt = cnt + 1
+        
+        fir = -5
+        for i in range(cnt):
+            deitem = re.search('href="(.+?)"',str(a[fir+5]))
+            if deitem:
+                deitem = deitem.group(1)
+                print(itemname[i].text.strip() + " : https:" + deitem)
+            fir+=5
+
+        '''
+        print('상세 제품1(' + itemname[0].text.strip() + ") : https:" + deitemimg1)
+        print('상세 제품2(' + itemname[1].text.strip() + ") : https:" + deitemimg2)
+        print('상세 제품3(' + itemname[2].text.strip() + ") : https:" + deitemimg3)
+        print('상세 제품3(' + itemname[3].text.strip() + ") : https:" + deitemimg4)
+        print(str(a))
+        print(cnt)
+        '''
         print("\n\n")
 
         
